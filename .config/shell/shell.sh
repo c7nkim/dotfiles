230d616ffc632fe9989274c5c261aaf52f9ef6b2
c7nkim <c7nkim@gmail.com>
2025-10-03 17:07:01 -0700

    fix: typo in linuxbrew shellenv

--- .config/shell/shell.sh
+++ .config/shell/shell.sh
@@ -6,7 +6,7 @@
 if [ -d "/opt/homebrew" ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shelenv)"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 # path
